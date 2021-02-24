efb877d6ac5195ef0c687bab08b067eda3b87770
jasonGMU <79413685+jasonGMU@users.noreply.github.com>
2021-02-23 19:15:53 -0600

    Update InventoryMgmt.py

--- InventoryMgmt.py
+++ InventoryMgmt.py
@@ -3,16 +3,20 @@ from tkinter import *
 root = Tk()
 root.title('Inventory Management - by MetSystems Inc.')
 root.iconbitmap('C:/Users/Jason/Documents/GMU/2021-01-Spring/IT-493_Capstone/ProjectFiles/favicon.ico')
+root.geometry("800x950")
+root.config(bg="#BDBDBD")
 
 
 topFrame = Frame(root)
-topFrame.pack()
+topFrame.config(bg="#000000")
+topFrame.pack(pady=50)
 
 bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
+bottomFrame.config(bg="#BDBDBD")
+bottomFrame.pack()
 
 
-header = Label(topFrame, text="Inventory Management", fg="#003B6D", bg="#BDBDBD", font="Verdana 36 bold")
+header = Label(topFrame, text="Inventory Management", height=2, width=100, fg="#003B6D", bg="#BDBDBD", font="Verdana 36 bold")
 header.pack()
 
 def openAdd():
@@ -22,10 +26,19 @@ def openAdd():
     #test label
     test_label = Label(top, text="Add New Item", fg="#003B6D", bg="#BDBDBD", font="Verdana 36 bold").pack()
 
+def openView():
+    top = Toplevel()
+    top.title('View All')
+    top.iconbitmap('C:/Users/Jason/Documents/GMU/2021-01-Spring/IT-493_Capstone/ProjectFiles/favicon.ico')
+    #test label
+    test_label = Label(top, text="View All", fg="#003B6D", bg="#BDBDBD", font="Verdana 36 bold").pack()
 
-newItemBTN = Button(bottomFrame, text="Add New Item", padx=300, pady=10, command=openAdd).pack()
 
+newItemBTN = Button(bottomFrame, text="Add New Item", padx=200, pady=20, font="Verdana 16", command=openAdd)
+newItemBTN.pack(pady=100)
 
+viewAllBTN = Button(bottomFrame, text="View All", padx=235, pady=20, font="Verdana 16", command=openView)
+viewAllBTN.pack(pady=100)
 
 
 
